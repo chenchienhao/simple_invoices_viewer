0dad519faf790409bb571ef6cd7dba7bf6f0c645
Hao <animax801213@gmail.com>
2019-06-24 14:03:48 -0400

    server.js

--- server.js
+++ server.js
@@ -74,4 +74,13 @@ app.post('/upload', (req, res) => {
   });
 });
 
-app.listen(Port, () => console.log('Server Started...'));
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(Port, () => console.log('Server Started...'+`Listening on port ${Port}`));
